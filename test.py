05143d7ea7bf8a8da3b204866f37c95519405c6f
Jiayang Tong <jiayangtong@uchicago.edu>
2015-04-20 18:22:04 -0500

    random pick

---
+++ test.py
@@ -0,0 +1,14 @@
+from __future__ import division
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#import Image
+import matplotlib.patches as mpatches
+from collections import deque
+import time
+import cv2
+
+input_image = plt.imread('train256/16_right.jpeg')
+plt.imshow(input_image)
+plt.show()
